054555be94471f9fff63d896d9eb6265daa67ac2
Emily Wessel <ettl02@gmail.com>
2018-07-09 23:32:33 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,25 +1,15 @@
 class MusicImporter
+  attr_reader :path
 
-  attr_accessor :path
-
-  def initialize(file_path)
-    @path = file_path
+  def initialize(path)
+    @path = path
   end
 
   def files
-    mp3_path = Dir.open(self.path)
-    mp3_files = mp3_path.map { |file| file }
-    2.times {mp3_files.shift}
-    mp3_files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
   def import
-    self.files.each do |file|
-      file = file.split(' - ')
-      song_name = file[1]
-      artist = Artist.find_or_create_by_name(file[0])
-      genre = Genre.find_or_create_by_name(file[2].split('.')[0])
-      Song.create(song_name, artist, genre)
-    end
+    files.each{ |f| Song.create_from_filename(f) }
   end
 end
