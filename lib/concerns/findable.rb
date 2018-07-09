5f969bb6f1bdee8db6c547127a9024f7c27b82b5
Emily Wessel <ettl02@gmail.com>
2018-07-09 23:17:02 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,11 @@
 module Concerns
   module Findable
-    
+
     def find_by_name(name)
       self.all.each {|song| return song if song.name == name}
       nil
     end
-  
+
     def find_or_create_by_name(name)
       self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
     end
